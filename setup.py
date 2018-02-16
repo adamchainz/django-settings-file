0d12c78bae45556d741495a9749c1c456b2830a1
Adam Johnson <me@adamj.eu>
2018-02-16 10:39:57 -0500

    Add python_requires to setup.py

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
     install_requires=[
         'Django',
     ],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     license='ISC',
     zip_safe=False,
     keywords='Django',
