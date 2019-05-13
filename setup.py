40ea1b94f7b255b244177aff062334a312f42562
Adam Johnson <me@adamj.eu>
2019-05-13 17:45:57 +0100

    Support Python 3.5 to 3.7 (#96)
    * Update requirements to compile one per Python version.
    * Update tox setup to test with all supported Python versions.
    * Update Travis config to use a matrix, with `tox-travis`.
    * Update README to say "Python 3.5-3.7 supported."
    * Update `classifiers` and `python_requires` in `setup.py`
    * Add note in `HISTORY.rst` "Update Python support to 3.5-3.7, as 3.4 has reached its end of life."

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     install_requires=[
         'Django>=1.11',
     ],
-    python_requires='>=3.4',
+    python_requires='>=3.5',
     license='ISC',
     zip_safe=False,
     keywords='Django',
@@ -49,8 +49,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 )
